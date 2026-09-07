1768c15dedee0d82c0b46c964f2ad19d223eedff
Sergio Veliz <sermi7626@gmail.com>
2026-09-07 10:32:44 -0500

    chore: inicializar proyecto Spring Boot y test RED inicial

---
+++ parkutp/src/main/java/com/utp/parkutp/ParkutpApplication.java
@@ -0,0 +1,13 @@
+package com.utp.parkutp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkutpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkutpApplication.class, args);
+	}
+
+}
---
+++ parkutp/src/test/java/com/utp/parkutp/ParkutpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.parkutp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkutpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
